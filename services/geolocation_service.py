9c2ed6c3dd89e3f7b85a99ec27cea0ea85b8a0f7
DiegoG0477 <diego.14.13.lopez@gmail.com>
2024-07-22 17:29:57 -0600

    validation coordinates added

--- services/geolocation_service.py
+++ services/geolocation_service.py
@@ -16,6 +16,7 @@ class GeolocationService:
         self.kit_id = None
         self.running = True
         self.current_coordinates = {'latitude': 0.0, 'longitude': 0.0}
+        self.coordinates_valid = False
         self.ser = serial.Serial(port, baudrate=baudrate, timeout=timeout)
 
     def start(self):
@@ -42,25 +43,23 @@ class GeolocationService:
                 newdata = self.ser.readline().decode('ascii', errors='replace').strip()
                 if newdata.startswith("$GPRMC"):
                     newmsg = pynmea2.parse(newdata)
-                    if isinstance(newmsg, pynmea2.RMC) and newmsg.status == 'A':
-                        self.current_coordinates = {'latitude': newmsg.latitude, 'longitude': newmsg.longitude}
+                    if isinstance(newmsg, pynmea2.RMC):
+                        if newmsg.status == 'A':
+                            self.current_coordinates = {'latitude': newmsg.latitude, 'longitude': newmsg.longitude}
+                            self.coordinates_valid = True
+                        else:
+                            self.coordinates_valid = False
                 time.sleep(1)
             except (serial.SerialException, pynmea2.ParseError, UnicodeDecodeError) as e:
                 print(f"Error reading GPS data: {e}")
+                self.coordinates_valid = False
 
     def send_coordinates_periodically(self):
         db_conn = self.database.get_connection()
         cursor = db_conn.cursor()
         while self.running:
-            point = f"POINT({self.current_coordinates['latitude']} {self.current_coordinates['longitude']})"
             try:
-                cursor.execute(
-                    "INSERT INTO geolocation (coordinates, geo_time) VALUES (ST_GeomFromText(%s), %s)",
-                    (point, datetime.now())
-                )
-                db_conn.commit()
                 coordinates = self.get_current_coordinates()
-                
                 message = json.dumps({
                     "kit_id": self.kit_id,
                     "driver_id": self.driver_id,
@@ -68,14 +67,33 @@ class GeolocationService:
                     "coordinates": coordinates
                 })
 
-                self.rabbitmq_service.send_message(message, "geolocation.update")
+                if self.coordinates_valid:
+                    point = f"POINT({self.current_coordinates['latitude']} {self.current_coordinates['longitude']})"
+                    cursor.execute(
+                        "INSERT INTO geolocation (coordinates, geo_time) VALUES (ST_GeomFromText(%s), %s)",
+                        (point, datetime.now())
+                    )
+                    db_conn.commit()
+                    self.rabbitmq_service.send_message(message, "geolocation.update")
+                else:
+                    print("GPS coordinates are not valid or sensor is calibrating.")
+                    self.rabbitmq_service.send_message(json.dumps({
+                        "kit_id": self.kit_id,
+                        "driver_id": self.driver_id,
+                        "datetime": datetime.now().isoformat(),
+                        "status": "GPS coordinates are not valid or sensor is calibrating."
+                    }), "geolocation.status")
+
             except Exception as e:
                 print(f"Error saving coordinates to DB or sending to RabbitMQ: {e}")
             time.sleep(3)
 
     def get_current_coordinates(self):
-        coordinates = self.current_coordinates
-        return f"POINT({coordinates['latitude']} {coordinates['longitude']})"
+        if self.coordinates_valid:
+            coordinates = self.current_coordinates
+            return f"POINT({coordinates['latitude']} {coordinates['longitude']})"
+        else:
+            return "Coordinates not valid or sensor calibrating"
 
 # Inicializar el servicio de geolocalización
 geolocation_service = GeolocationService()
