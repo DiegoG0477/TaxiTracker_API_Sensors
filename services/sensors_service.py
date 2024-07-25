a05528cbfb1ae8d2987745f77729a75324013fdc
DiegoG0477 <diego.14.13.lopez@gmail.com>
2024-07-25 00:51:59 -0600

    sensors thread

--- services/sensors_service.py
+++ services/sensors_service.py
@@ -1,15 +1,10 @@
-from datetime import datetime
 import threading
 import time
-from services.geolocation_service import geolocation_service
-from crash.controllers import register_crash
-from crash.models import CrashRequestModel
-from driving.models import DrivingModel
-from driving.controllers import register_driving
+import asyncio
+from datetime import datetime
 from gpiozero import InputDevice
 import smbus
 import logging
-import asyncio
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -24,7 +19,6 @@ class SensorService:
         self.running = True
         self.is_traveling = False
         self.lock = threading.Lock()
-        # self.thread = None
         self.task = None
         self.kit_id = None
         self.driver_id = None
@@ -42,8 +36,10 @@ class SensorService:
         self.start_time = self.millis()
         self.vibrations = 0
         self.shocks = 0
-
         self.last_shock_time = self.millis()
+
+        self.sensor_thread = threading.Thread(target=self.process_sensor_data)
+        self.sensor_thread.daemon = True  # Hilo se cierra cuando el proceso principal termina
 
     def initialize_i2c(self):
         retry_count = 0
@@ -65,7 +61,8 @@ class SensorService:
         if not self.task:
             try:
                 logger.info("Starting sensor service...")
-                self.task = asyncio.create_task(self.process_sensor_data())
+                self.sensor_thread.start()  # Iniciar el hilo para sensores
+                self.task = asyncio.create_task(self.check_sensor_status())
             except Exception as e:
                 logger.error(f"Error starting sensor service: {e}")
 
@@ -79,6 +76,8 @@ class SensorService:
             except asyncio.CancelledError:
                 pass
             self.task = None
+        if self.sensor_thread.is_alive():
+            self.sensor_thread.join()
 
     async def start_travel(self, kit_id, driver_id):
         with self.lock:
@@ -156,13 +155,13 @@ class SensorService:
                     value = value - 65536
                 return value
             except OSError as e:
-                print(f"Error reading raw data (attempt {retry_count + 1}): {e}")
+                logger.error(f"Error reading raw data (attempt {retry_count + 1}): {e}")
                 retry_count += 1
                 time.sleep(self.RETRY_DELAY)
                 if retry_count == self.MAX_RETRIES:
-                    print(f"Failed to read raw data from address {addr} after multiple attempts")
-                    return 0  # or return a default value
-                self.initialize_i2c()  # Try to reinitialize the I2C bus
+                    logger.error(f"Failed to read raw data from address {addr} after multiple attempts")
+                    return 0
+                self.initialize_i2c()
 
     async def process_sensor_data(self):
         while self.running:
@@ -174,10 +173,9 @@ class SensorService:
                         data = self.read_sensors()
                         if data is not None:
                             sensor_data.append(data)
-                            # print("Sensor data:", data)
                             await self.check_for_collision(data)
                         else:
-                            print("Failed to read sensor data")
+                            logger.warning("Failed to read sensor data")
                         time.sleep(0.5)
                     
                     if sensor_data:
@@ -203,110 +201,48 @@ class SensorService:
                             )
                             
                             await register_driving(driving_data)
-                            print("Driving data:", driving_data)
+                            logger.info("Driving data:", driving_data)
                         except Exception as e:
-                            print(f"Error processing driving data: {e}")
+                            logger.error(f"Error processing driving data: {e}")
                 else:
-                    time.sleep(1)
+                    await asyncio.sleep(1)
 
     def calculate_averages(self, sensor_data):
         avg_data = {}
         
-        # a) Promedio de velocidad de aceleración en m/s
         accelerations = [d['acc_x'] for d in sensor_data if d['acc_x'] > 0]
         avg_data['avg_acceleration'] = sum(accelerations) / len(accelerations) if accelerations else 0
         
-        # b) Promedio de velocidad de desaceleración / frenado en m/s
         decelerations = [abs(d['acc_x']) for d in sensor_data if d['acc_x'] < 0]
         avg_data['avg_deceleration'] = sum(decelerations) / len(decelerations) if decelerations else 0
         
-        # c) Cantidad de vibraciones
-        avg_data['vibrations'] = sensor_data[-1]['vibrations']
+        inclinations = [d['angle'] for d in sensor_data]
+        avg_data['avg_inclination_angle'] = sum(inclinations) / len(inclinations) if inclinations else 0
         
-        # d) Promedio de velocidad angular
-        avg_data['avg_angular_velocity'] = sum(abs(d['gyro_x']) + abs(d['gyro_y']) + abs(d['gyro_z']) for d in sensor_data) / (3 * len(sensor_data))
+        velocities = [d['gyro_x'] for d in sensor_data]
+        avg_data['avg_angular_velocity'] = sum(velocities) / len(velocities) if velocities else 0
         
-        # e) Promedio de ángulo de inclinación (sobre el eje z)
-        avg_data['avg_inclination_angle'] = sum(d['angle'] for d in sensor_data) / len(sensor_data)
+        avg_data['vibrations'] = sensor_data[0]['vibrations'] if sensor_data else 0
+        avg_data['avg_g_force_x'] = sum(d['g_force_x'] for d in sensor_data) / len(sensor_data) if sensor_data else 0
+        avg_data['avg_g_force_y'] = sum(d['g_force_y'] for d in sensor_data) / len(sensor_data) if sensor_data else 0
         
-        # f) Promedio de fuerzas G en eje X e Y
-        avg_data['avg_g_force_x'] = sum(d['g_force_x'] for d in sensor_data) / len(sensor_data)
-        avg_data['avg_g_force_y'] = sum(d['g_force_y'] for d in sensor_data) / len(sensor_data)
-        
-
         return avg_data
-
-    def calculate_g_force(self, Ax, Ay, Az):
-        # Calcula la fuerza G basada en las aceleraciones
-        return (Ax**2 + Ay**2 + Az**2)**0.5
-
-    async def check_for_collision(self, data):
-        collision_detected = False
-        relevant_data = {
-            "shocks": data['shocks'],
-            "g_force_x": data['g_force_x'],
-            "g_force_y": data['g_force_y'],
-            "g_force": data['g_force']
-        }
-
-        coordinates = await geolocation_service.get_current_coordinates_async()
-        if not coordinates or coordinates == 'Coordinates not valid or sensor calibrating':
-            coordinates = "..."  # Valor predeterminado si las coordenadas no son válidas
-
-        crash_data = CrashRequestModel(
-            kit_id=self.kit_id,
-            driver_id=self.driver_id,
-            datetime=datetime.now(),
-            impact_force=data['g_force'],
-            crash_coordinates=coordinates
-        )
-
-        # if data['g_force'] > self.G_FORCE_THRESHOLD:
-        #     collision_detected = True
-        #     register_crash(crash_data)
-        # elif self.shock_ky031.is_active:
-        #     collision_detected = True
-        #     print('colission detected by shock sensor')
-        #     register_crash(crash_data)
-        if data['g_force'] > self.G_FORCE_THRESHOLD:
-            collision_detected = True
-            await register_crash(crash_data)
-
-        if collision_detected:
-            print("Collision detected:", relevant_data)
-
-    def MPU_Init(self):
-        # Inicialización del MPU6050
-        self.bus.write_byte_data(self.Device_Address, self.SMPLRT_DIV, 7)
-        self.bus.write_byte_data(self.Device_Address, self.PWR_MGMT_1, 1)
-        self.bus.write_byte_data(self.Device_Address, self.CONFIG, 0)
-        self.bus.write_byte_data(self.Device_Address, self.GYRO_CONFIG, 24)
-        self.bus.write_byte_data(self.Device_Address, self.INT_ENABLE, 1)
-
-    def read_raw_data(self, addr):
-        # Leer datos crudos del MPU6050
-        high = self.bus.read_byte_data(self.Device_Address, addr)
-        low = self.bus.read_byte_data(self.Device_Address, addr + 1)
-        value = ((high << 8) | low)
-        if value > 32768:
-            value = value - 65536
-        return value
 
     def millis(self):
         return int(round(time.time() * 1000))
 
-    # Direcciones y registros del MPU6050
-    PWR_MGMT_1 = 0x6B
-    SMPLRT_DIV = 0x19
-    CONFIG = 0x1A
-    GYRO_CONFIG = 0x1B
-    INT_ENABLE = 0x38
-    ACCEL_XOUT = 0x3B
-    ACCEL_YOUT = 0x3D
-    ACCEL_ZOUT = 0x3F
-    GYRO_XOUT = 0x43
-    GYRO_YOUT = 0x45
-    GYRO_ZOUT = 0x47
+    def calculate_g_force(self, x, y, z):
+        return (x ** 2 + y ** 2 + z ** 2) ** 0.5
 
-# Create an instance of SensorService
+    async def check_for_collision(self, data):
+        if data['vibrations'] > self.G_FORCE_THRESHOLD:
+            logger.info("Collision detected!")
+            # Aquí puedes manejar la detección de colisiones si es necesario
+
+    async def check_sensor_status(self):
+        while self.running:
+            logger.info("Sensor service running...")
+            await asyncio.sleep(10)
+
+# Crear una instancia del servicio
 sensor_service = SensorService()
