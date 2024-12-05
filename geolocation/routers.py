258c5769d47f47e564fa48a8766b8b22ab85962f
yazreyesrz <ryazru1993@gmail.com>
2024-12-05 01:29:34 -0600

    model threads updated

--- geolocation/routers.py
+++ geolocation/routers.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Query
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from services.gpio_service import gpio_service
 from geolocation.models import GeolocationEntityModel
+from geolocation.controllers import predict_heatmap
 
 router = APIRouter()
 
@@ -21,3 +22,15 @@ async def get_location_api():
         location = GeolocationEntityModel(lat=0, long=0)
 
     return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder(location))
+
+@router.get("/heatmap")
+async def get_heatmap(
+    hour: int = Query(..., ge=0, le=23),
+    day_of_week: int = Query(..., ge=0, le=6),
+    latitude: float = Query(...),
+    longitude: float = Query(...),
+):
+    """
+    Endpoint para generar mapas de calor basados en los últimos modelos generados.
+    """
+    return await predict_heatmap(hour, day_of_week, latitude, longitude)
